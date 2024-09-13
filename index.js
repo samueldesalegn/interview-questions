ed028cba6d72649a801f5d12e06bc9af3738acf0
samueldesalegn <samueldesalegnt@gmail.com>
2024-09-13 11:11:52 -0400

    is_palidrome solved in two lines of code

---
+++ index.js
@@ -0,0 +1,9 @@
+function isPalindrome(str) {
+    var transformed = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return transformed === transformed.split("").reverse().join("");
+}
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello")); // false
+console.log(isPalindrome("Was it a car or a cat I saw?")); // true
+console.log(isPalindrome("No 'x' in Nixon")); // true
